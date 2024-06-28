2472fd1dfe5398b9ec1cd2f5beba28b72b451dc2
Han Myat Thu <hanmyatthu111@gmail.com>
2024-06-28 20:31:29 +0700

    initialize project

---
+++ providers/theme-provider.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+
+interface CustomThemeProviderProps extends ThemeProviderProps {
+  children: React.ReactNode
+}
+
+export function ThemeProvider({
+  children, 
+  ...props
+}: CustomThemeProviderProps) {
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+    </NextThemesProvider>
+  )
+}
