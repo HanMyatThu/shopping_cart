65e173b8c8f0b39b000a9b69f3c2a14fc165e28b
Han Myat Thu <hanmyatthu111@gmail.com>
2024-06-28 23:02:35 +0700

    create context for statem management

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
-import { ItemListCarousel } from "@/components/items/item-list-carousel"
-import { Navigation } from "@/components/navigations"
+'use client';
+
+import { CartProvider } from "@/contexts/cartProvider"
 import { data } from "@/data/data"
 import { ItemInterface } from "@/interfaces/item-interface"
+
+import { ItemListCarousel } from "@/components/items/item-list-carousel"
+import { Navigation } from "@/components/navigations"
 
 const App = () => {
 
   const bestSelection = data.filter((data: ItemInterface) => data.bestSelection)
 
   return (
-    <div>
+    <CartProvider>
       <Navigation />
       <ItemListCarousel title="Best Selections" items={bestSelection}/>
 
       {/* All Products */}
       <ItemListCarousel title="All Products" items={data}/>
-    </div>
+    </CartProvider>
   )
 }
 
---
+++ contexts/cartProvider.tsx
@@ -0,0 +1,86 @@
+import React, { createContext, useContext, useState } from "react";
+
+import { ItemInterface } from "@/interfaces/item-interface";
+
+interface CartItemProps {
+  id: string,
+  title: string,
+  price: number,
+  quantity: number,
+}
+
+interface CartContextProps {
+  items: CartItemProps[],
+  addCartItem: (id: ItemInterface) => void
+  removeCartItem: (id: string) => void
+  updateCartItem: (id: string) => void
+  clearCartItem: () => void
+}
+
+interface CartProviderProps {
+  children: React.ReactNode
+}
+
+const CartContext = createContext<CartContextProps | undefined>(undefined)
+
+
+export const CartProvider = ({
+  children
+}: CartProviderProps) => {
+
+  const [items, setItems] = useState<CartItemProps[]>([])
+
+  const addCartItem = (item: ItemInterface) => {
+    const isItemExisted = items.filter((i) => i.id === item.id)
+    if (isItemExisted) {
+      const updatedItems = items.map((i: CartItemProps) => i.id === item.id ? {...i, quantity: i.quantity += 1 }: {...i})
+      setItems(updatedItems)
+    } else {
+      const newItem = {
+        id: item.id,
+        title: item.title,
+        price: item.price,
+        quantity: 1,
+      } satisfies CartItemProps
+      const updatedItems = [...items, newItem]
+      setItems(updatedItems)
+    }
+  }
+
+  const removeCartItem = (id: string) => {
+    const filteredItems = items.filter((i) => i.id !== id)
+    setItems(filteredItems)
+  }
+
+  const updateCartItem = (id: string) => {
+    const updatedItems = items.map((i: CartItemProps) => i.id === id ? {...i, quantity: i.quantity += 1 }: {...i})
+    setItems(updatedItems)
+  }
+
+  const clearCartItem = () => {
+    setItems([])
+  }
+
+  const value = {
+    items,
+    addCartItem,
+    removeCartItem,
+    clearCartItem,
+    updateCartItem,
+  }
+
+
+  return (
+    <CartContext.Provider value={value}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export const useCartContext = () => {
+  const context = useContext(CartContext)
+  if (!context) {
+    throw new Error("UseCartContext must be used inside CartProvider")
+  }
+  return context;
+}
